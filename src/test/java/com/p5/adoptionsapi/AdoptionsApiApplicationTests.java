f0bbf9d3f0fc53c3f558658af376297a4604b3c1
RARES <rarescostea20@yahoo.com>
2021-07-26 15:18:38 +0300

    Added controller

---
+++ src/test/java/com/p5/adoptionsapi/AdoptionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p5.adoptionsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
